936f527d5b7aec2e88699bdc48cecb9d6c5c7190
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-05-13 13:40:33 +0300

    Time: 54 ms (88.29%), Space: 61.4 MB (24.58%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+    int start = 0, end = height.size() - 1, maxx = 0;
+    while (start<=end)
+    {
+        int mini = min(height[start], height[end]);
+        maxx = max(maxx, mini * (end - start));
+        if (height[start] < height[end])
+            start++;
+        else
+            end--;
+    }
+    return maxx;
+}
+};
