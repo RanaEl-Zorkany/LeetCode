b1f9d45f5139cdb081e426915d130413d759ce31
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-05-22 02:14:56 +0300

    Time: 0 ms (100.00%), Space: 8.3 MB (81.13%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> subsets;
+        // generating subsets
         for (int msk = 0; msk < (1 << n); msk++){
             vector<int> subset;
             for (int i = 0; i < n; i++)
