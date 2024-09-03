f77aac7916b1a72019fa13b175e8f2f7db95200a
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-09-03 11:35:40 +0300

    Time: 5 ms (25.74%), Space: 9.9 MB (47.02%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string numericString = "";
+        for(char ch:s){
+            numericString += to_string(ch - 'a' + 1);
+        }
+        while (k--)
+        {
+            int sum = 0;
+            for (int i = 0; i < numericString.size(); i++)
+            {
+                sum += numericString[i] - '0';
+            }
+            numericString = to_string(sum);
+        }
+        return stoi(numericString);
+    }
+};
