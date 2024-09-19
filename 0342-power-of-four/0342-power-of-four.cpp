9d7423c8306aab42905315cb23cc5122ae5bd5fd
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-09-19 21:05:26 +0300

    Time: 0 ms (100.00%), Space: 7.5 MB (73.55%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n == 1) return true;
-        if (n < 4) return false; 
+        if (n <= 0) return false; 
         while (n % 4 == 0) {
-            // if (n >= 4)
-                n /= 4; 
-            // else return false;
+            n /= 4; 
         }
         return (n == 1);
         
