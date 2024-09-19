0618de23c864d4a1d36b42077df1e4e2795486ab
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-09-19 20:50:00 +0300

    Time: 0 ms (100.00%), Space: 7.6 MB (27.22%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfTwo(int n) {
         if (n<=0) return false;
         double x = log10(n)/log10(2);
-        int res = (int)x;
+        int res = x/1;
         return (x==res) ? true : false;
     }
 };
