3085f286fada29d058b8abb64bd3a72384f762d3
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-10-04 06:29:56 +0300

    Time: 0 ms (100.00%), Space: 8.9 MB (21.19%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // BACKTRACKING
     long long ans;
     vector<int> taken;
     int subsetXORSum(vector<int>& nums) {
@@ -21,4 +22,25 @@ public:
         backtracking(nums, index + 1);
         taken.pop_back();
     }
+    
+    // BIT MANIPULATION
+    
+    // int subsetXORSum(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int result = 0;
+    //     for (int msk = 0; msk < (1 << n); msk++){
+    //         vector<int> subset;
+    //         for (int i = 0; i < n; i++)
+    //         {
+    //             if(msk & (1 << i))
+    //                 subset.push_back(nums[i]);
+    //         }
+    //         int sum = 0;
+    //         for(auto sub:subset){ 
+    //             sum ^= sub; 
+    //         }
+    //         result += sum;
+    //     }
+    //     return result;
+    // }
 };
