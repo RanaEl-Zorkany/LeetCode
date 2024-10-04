bd0bc85a677312df90867def0aa9087caf55114f
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-10-04 07:51:35 +0300

    Time: 0 ms (100.00%), Space: 11.5 MB (32.73%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> subsets;
-        // generating subsets
-        for (int msk = 0; msk < (1 << n); msk++){
-            vector<int> subset;
-            for (int i = 0; i < n; i++)
-            {
-                if(msk & (1 << i))
-                    subset.push_back(nums[i]);
-            }
-            subsets.push_back(subset);
-        }
-        return subsets;
+    vector<int> taken;
+    vector<vector<int>> result;
+    vector<vector<int>> subsets(vector<int>& nums){
+        int index = 0;
+        backtracking(nums, index);
+        return result;
     }
+    void backtracking(vector<int>& nums, int index){
+        if(index == nums.size()){
+            result.push_back(taken);
+            return;
+        }
+        backtracking(nums, index + 1);
+        taken.push_back(nums[index]);
+        backtracking(nums, index + 1);
+        taken.pop_back();
+    }
+    
+    
+    // vector<vector<int>> subsets(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<vector<int>> subsets;
+    //     // generating subsets
+    //     for (int msk = 0; msk < (1 << n); msk++){
+    //         vector<int> subset;
+    //         for (int i = 0; i < n; i++)
+    //         {
+    //             if(msk & (1 << i))
+    //                 subset.push_back(nums[i]);
+    //         }
+    //         subsets.push_back(subset);
+    //     }
+    //     return subsets;
+    // }
 };
