fa0f6b0d40131014144fcd246eda44045b14788f
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-03 15:38:54 +0200

    Time: 0 ms (100.00%), Space: 7.6 MB (55.81%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,6 +3,6 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size()) return false;
         string doubleString = s + s;
-        return doubleString.find(goal) != string::npos;
+        return doubleString.find(goal) != -1 /*string::npos*/;
     }
 };
