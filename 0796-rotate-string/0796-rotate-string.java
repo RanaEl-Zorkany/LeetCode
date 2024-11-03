0ad0706624fcbc7f4fd70c78560c475a6b27c197
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-03 15:33:41 +0200

    Time: 0 ms (100.00%), Space: 41 MB (89.26%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         if (s.length() != goal.length()) return false;
-        String doubleString = s += s;
+        String doubleString = s + s;
         return doubleString.indexOf(goal) != -1;
     }
 }
