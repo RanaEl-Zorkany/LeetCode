3bf7c5c127d6c7c121311f789258d5a018a10d8d
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-06 11:50:38 +0200

    Time: 3 ms (48.57%), Space: 44.3 MB (48.43%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        int SIZE = nums.length;
+        for (int i = 0; i < SIZE - 1; i++)
+        {
+            for (int j = i + 1; j < SIZE; j++)
+            {
+                if (nums[i] > nums[j]){
+                    if(Integer.bitCount(nums[i]) == Integer.bitCount(nums[j])){
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                    else return false;
+                }
+                else continue;
+            }
+        }
+        return true;
+    }
+}
