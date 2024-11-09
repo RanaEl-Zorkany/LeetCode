6eb0ba7564e7558d3bd4505019c30ec36a652c30
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-09 21:52:50 +0200

    Time: 6 ms (5.36%), Space: 43.6 MB (6.09%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if (n == 1)
+            return true;
+
+        else{
+            int it = 100;
+            while (it > 0){
+                String string_num = Integer.toString(n);
+                int squared = 0;
+                int len = string_num.length();
+                for(int i = 0; i<len; i++){
+                    squared += Math.pow(Integer.parseInt(String.valueOf(string_num.charAt(i))), 2);
+                }
+                n = squared;
+                if (n == 1)
+                    return true;
+                it--;
+            }
+        }
+        return n == 1;
+    }
+}
