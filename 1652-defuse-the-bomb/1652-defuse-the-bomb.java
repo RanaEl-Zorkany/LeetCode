a499cfd66e690b26509eee42e853e26837e5292a
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-18 15:24:51 +0200

    Time: 2 ms (67.36%), Space: 42.4 MB (13.75%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -4,15 +4,15 @@ class Solution {
         int[] result = new int[len];
         if(k == 0) return result;
         for(int i = 0; i < len; i++){
-            int cnt = 0, sum = 0;
+            int sum = 0;
             if (k > 0){
-                for(int j = i+1; cnt < k; j++){
+                for(int j = i+1; j < i + k + 1; j++){
                     sum += code[j % len];
-                    cnt++;
                 }
                 result[i] = sum;
             }
             else{
+                int cnt = 0;
                 for(int j = i-1; cnt > k; j--){
                     sum += code[(j + len) % len];
                     cnt--;
