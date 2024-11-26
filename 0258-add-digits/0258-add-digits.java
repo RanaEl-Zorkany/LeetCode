bef364b7e093f880a06879ecb388f6abe40fd336
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-26 06:39:01 +0200

    Time: 1 ms (49.31%), Space: 41.2 MB (9.53%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        while (num > 9) {
+            int newNum = num, ans = 0; 
+            while(newNum > 0){
+                ans = ans + (newNum % 10);
+                newNum = newNum / 10;
+            }
+            num = ans;
+        }
+        return num;
+    }
+}
