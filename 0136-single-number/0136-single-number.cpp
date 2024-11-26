6030cb310515eef6b9dc28eff13ba42a21724062
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-26 06:13:40 +0200

    Time: 16 ms (10.53%), Space: 24.4 MB (5.19%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> mp;
+        for (int i = 0; i < nums.size(); i++)
+            mp[nums[i]]++;
+        map<int, int>::iterator it = mp.begin();
+        while (it != mp.end()) {
+            if(it -> second == 1)
+                return it -> first;
+            ++it;
+        }
+        return -1;
+    }
+};
