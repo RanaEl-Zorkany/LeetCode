155f281a2bbd0b5fe8a4043175f0428228fefa75
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-26 04:40:21 +0200

    Time: 3 ms (65.45%), Space: 22.7 MB (78.67%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        set<char> st; 
+        int cnt = 0;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                char digit = board[i][j];
+                if(digit != '.'){
+                     // Checks Horizontally
+                    for(int h = 0; h < 9; h++){
+                        if(h == j || board[i][h] == '.') continue;
+                        if(digit == board[i][h]) return false;
+                    }
+                    // Checks Vertically
+                    for(int v = 0; v < 9; v++){
+                        if(v == i ||  board[v][j] == '.') continue;
+                        if(digit == board[v][j]) return false;
+                    }
+                }
+               
+                // Same Square
+                if(i % 3 == 0 && j % 3 == 0){
+                    for(int os = i; os < i + 3; os++){
+                        for(int is = j; is < j + 3; is++){
+                            if(board[os][is] == '.'){
+                                cnt++;
+                                continue;
+                            }
+                            st.insert(board[os][is]);
+                        }
+                    }
+                    if(st.size() + cnt < 9) return false;
+                    st.clear();
+                    cnt = 0;
+                }
+            }
+        }
+        return true;
+    }
+};
