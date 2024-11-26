09877abd403e1e9abc526fefe70a816696156934
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-11-26 05:37:36 +0200

    Time: 4 ms (25.69%), Space: 95.5 MB (39.30%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    long long start1 = 0, start2 = 0;
-    vector<int> merged;
-    while (start1 < nums1.size() && start2 < nums2.size()) {
-        if (nums1[start1] <= nums2[start2]) {
+        long long start1 = 0, start2 = 0;
+        vector<int> merged;
+        while (start1 < nums1.size() && start2 < nums2.size()) {
+            if (nums1[start1] <= nums2[start2]) {
+                merged.push_back(nums1[start1]);
+                start1++;
+            }
+            else {
+                merged.push_back(nums2[start2]);
+                start2++;
+            }
+        }
+
+        while (start1 < nums1.size()) {
             merged.push_back(nums1[start1]);
             start1++;
         }
-        else {
+
+        while (start2 < nums2.size()) {
             merged.push_back(nums2[start2]);
             start2++;
         }
-    }
 
-    while (start1 < nums1.size()) {
-        merged.push_back(nums1[start1]);
-        start1++;
-    }
+        int size = merged.size();
 
-    while (start2 < nums2.size()) {
-        merged.push_back(nums2[start2]);
-        start2++;
-    }
-    
-    int size = merged.size();
-
-    if (size % 2 == 0) {
-        return (merged[size / 2 - 1] + merged[size / 2]) / 2.0;
-    }
-    else {
-        return merged[size / 2];
-    }
-    return 0;
+        if (size % 2 == 0) 
+            return (merged[size / 2 - 1] + merged[size / 2]) / 2.0;
+        else return merged[size / 2];
     }
 };
