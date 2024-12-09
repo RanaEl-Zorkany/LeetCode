8f089ee911bf0062d5bfab815a1ad839e6476121
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-12-10 00:13:33 +0200

    Time: 1 ms (86.76%), Space: 40.8 MB (48.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,14 +2,13 @@ class Solution {
     public int mySqrt(int x) {
         if (x <= 1) return x;
         
-        int start = 1, end = x; 
-        long mid = 1l;
+        long start = 1l, end = (long)x, mid = 1l;
         while (start <= end){
             mid = start + (end - start) / 2;
             if(mid * mid == x) return (int)mid;
-            else if (mid * mid < x) start = (int)mid + 1;
-            else end = (int)mid - 1;
+            else if (mid * mid < x) start = mid + 1;
+            else end = mid - 1;
         }
-        return end;
+        return (int)end;
     }
 }
