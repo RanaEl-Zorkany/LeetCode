8c644464322c5c2eaf3639bec5ea670504cc617b
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-12-17 03:07:57 +0200

    Time: 0 ms (100.00%), Space: 12.1 MB (19.09%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return "";
-        
         string base = strs[0];
         for(int i = 0; i < base.size(); i++){
-            string r = base.substr(0, i + 1);
+            string prefix = base.substr(0, i + 1);
             for(int j = 1; j < strs.size(); j++){
-                if(strs[j].find(r) != 0){
+                if(strs[j].find(prefix) != 0){
                     return base.substr(0, i);
                 }
             }
-        }
+        } 
+        // Longest Common Prefix
         return base;
     }
 };
