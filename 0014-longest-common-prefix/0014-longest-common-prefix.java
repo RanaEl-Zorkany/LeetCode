4b16c73159d5dbc33e5ec8bfa5b4e057a2319a9a
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2024-12-17 03:08:23 +0200

    Time: 1 ms (65.01%), Space: 41.8 MB (32.46%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,9 +4,9 @@ class Solution {
         
         String base = strs[0];
         for(int i = 0; i < base.length(); i++){
-            String r = base.substring(0, i + 1);
+            String prefix = base.substring(0, i + 1);
             for(int j = 1; j < strs.length; j++){
-                if(!strs[j].startsWith(r)){
+                if(!strs[j].startsWith(prefix)){
                     return base.substring(0, i);
                 }
             }
