ce9697c650865e3792e5b0e6352dc8107f3067fe
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-01-20 12:32:49 +0200

    Time: 0 ms, Memory: 32 MB - LeetHub

--- 13104-474-209-minimum-size-subarray-sum/13104-474-209-minimum-size-subarray-sum.cpp
+++ 13104-474-209-minimum-size-subarray-sum/13104-474-209-minimum-size-subarray-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
     vector<int> prefix;
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        // SLIDING WINDOW O(n)
+        // SLIDING WINDOW  Approach O(n) 
         int len = nums.size(), minLen = INT_MAX, start = 0, sum = 0;
         if(nums[0] >= target) return 1;
 
