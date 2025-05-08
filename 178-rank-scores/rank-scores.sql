820abeec04f115fb87db38a30e7988fa052d38a9
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-05-08 21:11:32 +0300

    Time: 328 ms (54.97%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,6 +1,3 @@
-/* Write your T-SQL query statement below */
-SELECT * FROM
-(
-    SELECT score, DENSE_RANK() OVER(ORDER BY score DESC) AS rank
-    FROM Scores
-) AS NEWTABLE
+# Write your MySQL query statement below
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS "rank" 
+FROM Scores;
