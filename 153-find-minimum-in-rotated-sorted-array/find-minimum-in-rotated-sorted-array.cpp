981a2c3e5a1ce9a35a3a5f3a87a7da4ffdafcfda
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-05-09 05:54:43 +0300

    Time: 0 ms (100.00%) | Memory: 14.2 MB (48.30%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -5,16 +5,12 @@ public:
         while(start <= end){
             mid = start + (end - start) / 2;
             if(nums[mid] >= nums[start]){
-                if(nums[end] >= nums[mid])
-                    end = mid - 1;
+                if(nums[end] >= nums[mid]) end = mid - 1;
                 else start = mid + 1;
             } 
-            else{
-                end = mid - 1;
-            }
+            else end = mid - 1;
             minn = min(minn, nums[mid]);
         }
-    
         return minn;
     }
 };
