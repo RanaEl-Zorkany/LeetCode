ea737e303de8733468ae29497abc0045944b08bc
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-05-11 17:54:23 +0300

    Time: 3 ms (78.18%) | Memory: 17.2 MB (20.01%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -2,18 +2,30 @@ class Solution {
 public:
     string largestNumber(vector<int>& nums) {
         string answer = "";
-        auto comp = [](const string &a, const string &b){
-        return a + b < b + a;
-        };
-        priority_queue<string, vector<string>, decltype(comp)> maxHeap(comp);
+        // auto comp = [](const string &a, const string &b){
+        // return a + b < b + a;
+        // };
+        // priority_queue<string, vector<string>, decltype(comp)> maxHeap(comp);
+        // for(auto num : nums){
+        //     maxHeap.push(to_string(num));
+        // }
+        // if(maxHeap.top() == "0") return "0";
+        // while(!maxHeap.empty()){
+        //     answer += maxHeap.top();
+        //     maxHeap.pop();
+        // }
+        // return answer;   
+        vector<string> str;
         for(auto num : nums){
-            maxHeap.push(to_string(num));
+            str.push_back(to_string(num));
         }
-        if(maxHeap.top() == "0") return "0";
-        while(!maxHeap.empty()){
-            answer += maxHeap.top();
-            maxHeap.pop();
+        sort(str.begin(), str.end(), [](const string &a, const string &b){
+            return a + b > b + a;
+        });
+        if(str[0] == "0") return "0";
+        for(auto num : str){
+            answer += num;
         }
-        return answer;      
+        return answer;   
     }
 };
