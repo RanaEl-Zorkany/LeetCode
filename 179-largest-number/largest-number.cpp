c079a29675bf4e4619dac4769badac3528c3b998
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-05-11 17:48:44 +0300

    Time: 3 ms (78.18%) | Memory: 16.8 MB (68.23%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
+        string answer = "";
         auto comp = [](const string &a, const string &b){
         return a + b < b + a;
         };
         priority_queue<string, vector<string>, decltype(comp)> maxHeap(comp);
-        string answer = "";
         for(auto num : nums){
             maxHeap.push(to_string(num));
         }
+        if(maxHeap.top() == "0") return "0";
         while(!maxHeap.empty()){
             answer += maxHeap.top();
             maxHeap.pop();
         }
-        return answer[0] == '0' ? "0" : answer;
+        return answer;      
     }
 };
