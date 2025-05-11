f31263708156aff34561034bb5647c354f19420f
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-05-11 17:55:14 +0300

    Time: 3 ms (78.18%) | Memory: 17 MB (58.45%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
+        /* WITH HEAP */
         string answer = "";
-        // auto comp = [](const string &a, const string &b){
-        // return a + b < b + a;
-        // };
-        // priority_queue<string, vector<string>, decltype(comp)> maxHeap(comp);
-        // for(auto num : nums){
-        //     maxHeap.push(to_string(num));
-        // }
-        // if(maxHeap.top() == "0") return "0";
-        // while(!maxHeap.empty()){
-        //     answer += maxHeap.top();
-        //     maxHeap.pop();
-        // }
-        // return answer;   
-        vector<string> str;
+        auto comp = [](const string &a, const string &b){
+        return a + b < b + a;
+        };
+        priority_queue<string, vector<string>, decltype(comp)> maxHeap(comp);
         for(auto num : nums){
-            str.push_back(to_string(num));
+            maxHeap.push(to_string(num));
         }
-        sort(str.begin(), str.end(), [](const string &a, const string &b){
-            return a + b > b + a;
-        });
-        if(str[0] == "0") return "0";
-        for(auto num : str){
-            answer += num;
+        if(maxHeap.top() == "0") return "0";
+        while(!maxHeap.empty()){
+            answer += maxHeap.top();
+            maxHeap.pop();
         }
-        return answer;   
+
+        /* WITHOUT HEAP */
+        // vector<string> str;
+        // for(auto num : nums){
+        //     str.push_back(to_string(num));
+        // }
+        // sort(str.begin(), str.end(), [](const string &a, const string &b){
+        //     return a + b > b + a;
+        // });
+        // if(str[0] == "0") return "0";
+        // for(auto num : str){
+        //     answer += num;
+        // }
+        return answer;           
     }
 };
