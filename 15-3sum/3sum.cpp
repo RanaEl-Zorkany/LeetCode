c90a1fff9443ba9a988291fa5ce8f2cd0a1f6050
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-06-21 09:37:00 +0300

    Time: 50 ms (57.05%) | Memory: 29 MB (74.03%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -10,8 +10,8 @@ public:
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0){
                     ans.push_back({nums[i], nums[left], nums[right]});
-                    while(left < right && nums[left] == nums[left + 1]) left++;
-                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    while(left < right && nums[left] == nums[left + 1]) left++; // skip duplicates
+                    while(left < right && nums[right] == nums[right - 1]) right--; // skip duplicates
                     left++; right--;
                 }
                 else if (sum > 0) right--;
