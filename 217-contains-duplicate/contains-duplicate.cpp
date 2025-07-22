3b25652d705b4823618d526d9d5467167168452e
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-07-22 06:28:15 +0300

    Time: 104 ms (11.14%) | Memory: 92.5 MB (23.29%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s;
+        for(int n:nums){
+            if(s.count(n)) return true;
+            s.insert(n);
+        }
+        return false;
+    }
+};
