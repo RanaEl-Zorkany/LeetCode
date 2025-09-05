c677f6190b962eb9a8844060e5cb8fd9637af3ed
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-09-05 14:19:06 +0300

    Time: 0 ms (100.00%) | Memory: 21.4 MB (68.05%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -10,30 +10,22 @@
  * };
  */
 class Solution {
-    int targetSum, target;
     bool found;
     bool isLeaf(TreeNode* root){
         return (!root->left && !root->right);
     }
-    void dfs(TreeNode* root, int sum){
-        if (!root || found) return;
-
-        if (isLeaf(root) && sum == targetSum) {
-            found = true;
-            return;
+    void dfs(TreeNode* root, int targetSum, int currentSum){
+        if(!root || found) return;
+        if(isLeaf(root) && currentSum == targetSum){
+            found = true; return;
         }
-        
-        cout << root->val << "  " << sum <<endl;
-        if(root->left)
-            dfs(root->left, sum + root->left->val);
-        if(root->right)
-            dfs(root->right, sum + root->right->val);
+        if(root->left) dfs(root->left, targetSum, currentSum + root->left->val);
+        if(root->right) dfs(root->right, targetSum, currentSum + root->right->val);
     }
 public:
     bool hasPathSum(TreeNode* root, int t) {
         if(!root) return false;
-        targetSum = t;
-        dfs(root, root->val);
+        dfs(root, t, root->val);
         return found;
     }
 };
