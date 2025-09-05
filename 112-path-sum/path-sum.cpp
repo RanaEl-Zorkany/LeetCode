7e890f1cd1af74102e14ce03bffd11e2d94e9501
Rana El-Zorkany <119603365+RanaEl-Zorkany@users.noreply.github.com>
2025-09-05 14:14:25 +0300

    Time: 47 ms (1.94%) | Memory: 21.6 MB (34.89%) - LeetSync

---
+++ 112-path-sum/path-sum.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int targetSum, target;
+    bool found;
+    bool isLeaf(TreeNode* root){
+        return (!root->left && !root->right);
+    }
+    void dfs(TreeNode* root, int sum){
+        if (!root || found) return;
+
+        if (isLeaf(root) && sum == targetSum) {
+            found = true;
+            return;
+        }
+        
+        cout << root->val << "  " << sum <<endl;
+        if(root->left)
+            dfs(root->left, sum + root->left->val);
+        if(root->right)
+            dfs(root->right, sum + root->right->val);
+    }
+public:
+    bool hasPathSum(TreeNode* root, int t) {
+        if(!root) return false;
+        targetSum = t;
+        cout << root->val << endl;
+        dfs(root, root->val);
+        return found;
+    }
+};
